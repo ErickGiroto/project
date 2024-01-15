2d41aaa881f1f0dc75bcfe4bdc68e0894fd2d4dd
ErickGiroto <erick_giroto@hotmail.com>
2024-01-15 17:37:25 -0300

    Projeto Iniciado Xadrez Part 06

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,10 @@ public class Board { // Tabuleiro
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar o quadro: Tem de haver pelo menos 1 linha e 1 coluna");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,34 +20,60 @@ public class Board { // Tabuleiro
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
+	
 	// Posição da Linha e Coluna
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Posição que não está no quadro");			
+		}
 		return pieces[row][column];
 	}
 
+	
 	// Retornando a peça pela Posição
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição que não está no quadro");			
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	
 	// Atribuir a posicao a peça que veio como argumento
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPice(position)) {
+			throw new BoardException("Já existe uma peça na posição " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		
 		//Dizer que a peça não está na posição nula e informar que está na posição - 'posistion'
 		piece.position = position; // O Acesso ao piece só é possivel porque está no mesmo pacote boardgame
 	}
+	
+	
+	// Criando as defensivas do Game
+	public boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPice(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição que não está no quadro");			
+		}
+		return piece(position) != null;
+	}
+	
+	
+	
+	
+	
 
 }
